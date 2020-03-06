1757ebe142df64a3d8a89ce5430daee262987150
Lucas Silva <lsilvapg21@gmail.com>
2020-03-06 13:05:05 -0300

    Codigos em php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <form action= "index.php" method="post">
 Nome: <input type="text" name="Nome"/><br>
 Curso: <input type="text" name="Curso"><br>
-Data De Nascimento: <input type= "text" name="Data De Nascimento"><br>
+DataDeNascimento: <input type= "text" name="DataDeNascimento"><br>
 <input type=submit value="Ok">
 </form>
 
 <?php
 echo "Nome: " . $_POST["Nome"];
 echo "<br> Curso: " . $_POST["Curso"];
-echo "<br> Data De Nascimento: " . $_POST["Data De Nascimento"];
+echo "<br> Data De Nascimento: " . $_POST["DataDeNascimento"];
 ?>
 </html>
