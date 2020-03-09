19a0b6145a4df40b85fddb0af6e24fd8cd7b931b
Lucas Silva <lsilvapg21@gmail.com>
2020-03-09 15:45:32 -0300

    fomulario php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <html>
 <form action= "index.php" method="post">
-Nome: <input type="text" name="Nome"/><br>
-Curso: <input type="text" name="Curso"><br>
-DataDeNascimento: <input type= "text" name="DataDeNascimento"><br>
+ Nome: <input type="text" name="Nome"/><br>
+ Curso: <select name="Curso"><br>
+	<option value="Mecanica">Mecanica</option>
+	<option value="Eletrica">Eletrica</option>
+	<option value="Administracao">Administracao</option>
+	</select>
+	<br>
+ DataDeNascimento: <input type= "date" name="DataDeNascimento"><br>
 <input type=submit value="Ok">
 </form>
 
 <?php
 echo "Nome: " . $_POST["Nome"];
 echo "<br> Curso: " . $_POST["Curso"];
-echo "<br> Data De Nascimento: " . $_POST["DataDeNascimento"];
+echo "<br> Data De Nascimento: " .  date("d/m/Y", strtotime($_POST["DataDeNascimento"]));
 ?>
 </html>
